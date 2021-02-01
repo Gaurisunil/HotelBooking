4092c65c3f0d3350e9d454dd89812670be06f82f
Gaurisunil <62568493+Gaurisunil@users.noreply.github.com>
2021-02-02 03:36:29 +0530

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <hr />
+            <center>
+                <h3>&copy; Gauri Sunil</h3>
+            </center>
+
+        </footer>
+    )
+}
+
+export default Footer
